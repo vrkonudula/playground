1d4a26e5b2602c3cb9105b8fb063b40382ea61b3
Venkat <vkonudula@vt.edu>
2021-02-19 18:31:24 -0500

    Added second and third exercises

---
+++ src/pages/sample.js
@@ -0,0 +1,22 @@
+import '../css/sample.css';
+function sample() {
+    return (
+        <>
+            <div className='total'>
+                <div className="gri">1</div>
+                <div className="gri">2</div>
+                <div className="gri">3</div>
+                <div className="gri">4</div>
+                <div className="gri">5</div>
+                <div className="gri">6</div>   
+            </div>
+            {/* <div className="gri">
+                <div className="left"></div>
+                <div className="right"></div>
+            </div> */}
+            
+            
+        </>
+    )
+}
+export default sample;
