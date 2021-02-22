951653e410ad7574d85f1bf12c5eadf964b56c33
Venkat <vkonudula@vt.edu>
2021-02-22 16:44:19 -0500

    Added text changes to theme exercise

--- src/pages/DarkTheme.js
+++ src/pages/DarkTheme.js
@@ -1,14 +1,30 @@
-import '../theme.scss';
-import { useState } from 'react';
+import "../theme.scss";
+import { useState } from "react";
 function DarkTheme() {
-    const [th, setTh] = useState("lighttheme");
-    return (
-        <div className="e4content">
-            <div className={`${th}`}> Hello there</div>
-            <button onClick={() => setTh("lighttheme")}>Light Theme</button>
-            <button onClick={() => setTh("darktheme")}>Dark Theme</button>
-        </div>
-    )
+  const [th, setTh] = useState("lighttheme");
+  return (
+    <div className="e4content">
+      <div className={`${th}`}> You are in ligh theme!!</div>
+      <button
+        onClick={() => {
+          setTh("lighttheme");
+          document.getElementsByClassName(`${th}`)[0].innerHTML =
+            " You are in light theme!!";
+        }}
+      >
+        Light Theme
+      </button>
+      <button
+        onClick={() => {
+          setTh("darktheme");
+          document.getElementsByClassName(`${th}`)[0].innerHTML =
+            " You are in dark theme!!";
+        }}
+      >
+        Dark Theme
+      </button>
+    </div>
+  );
 }
 
 export default DarkTheme;
